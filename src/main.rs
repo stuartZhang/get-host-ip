a19de2a948084e26f26e00e91acc70298a3f9191
何彬龙 <hebinlong@bluefire.top>
2023-05-05 11:04:12 +0800

    使用 encoding 解码

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 //! 获取宿主机器的 IP 地址
 use ::clap::Parser;
+use ::encoding::all::{GB18030, UTF_8};
+use ::encoding::Encoding;
 use ::regex::Regex;
 use ::std::{collections::HashMap, error::Error, process::Command};
+
 fn main() -> Result<(), Box<dyn Error>> {
     /// 获取 wsl 宿主机器的物理 IP 地址
     #[derive(Parser, Debug)]
@@ -16,54 +19,70 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     let cli_args = CliArgs::parse();
     let output = Command::new("ipconfig.exe").output()?.stdout;
-    let ip_config_str = iconv_sys::decode(output.as_slice(), "cp936")?;
+    let r = chardet::detect(&output);
+    let en = encoding::label::encoding_from_whatwg_label(chardet::charset2encoding(&r.0))
+        .ok_or(format!("无法找到: {} 解码器", r.0))?;
+    let ip_config_str = en
+        .decode(&output, encoding::DecoderTrap::Strict)?;
+    // let ip_config_str = iconv_sys::decode(output.as_slice(), "cp936")?;
     #[cfg(debug_assertions)]
     println!("{ip_config_str}");
     let ip_config_pojo = build_map(&ip_config_str[..], None)?;
     #[cfg(debug_assertions)]
     dbg!(&ip_config_pojo);
-    ip_config_pojo.get(&cli_args.section[..]).and_then(|section| {
-        section.get(&cli_args.entry[..])
-    }).map(|value| {
-        print!("{value}");
-    });
+    if let Some(value) = ip_config_pojo
+        .get(&cli_args.section[..])
+        .and_then(|section| section.get(&cli_args.entry[..]))
+    {
+        print!("{value}")
+    }
     Ok(())
 }
-fn build_map(input_str: &str, separator: Option<&str>) -> Result<HashMap<String, HashMap<String, String>>, Box<dyn Error>> {
+
+#[allow(clippy::invalid_regex)]
+fn build_map(
+    input_str: &str,
+    separator: Option<&str>,
+) -> Result<HashMap<String, HashMap<String, String>>, Box<dyn Error>> {
     let section = Regex::new(r"^\S+")?;
     let record = Regex::new(r"^\s+")?;
     let rear_colon = Regex::new(r"^\s*|:\s*$")?;
-    let partition = Regex::new(r"(?x)
+    let partition = Regex::new(
+        r"(?x)
         \s*
         (?<key>[^\.]+)
         (?:\.\s)+:\s*
         (?<value>.+)
         \s*
-    ")?;
+    ",
+    )?;
     let mut last_section_key: Option<String> = None;
-    Ok(input_str.split(separator.or(Some("\r\n")).unwrap()).fold(HashMap::new(), |mut map, line| {
-        if section.is_match(line) {
-            let section_key = rear_colon.replace_all(line, "").into_owned();
-            map.entry(section_key.clone()).or_insert(HashMap::new());
-            last_section_key.replace(section_key);
-        } else if record.is_match(line) {
-            last_section_key.as_ref().and_then(|section_key| {
-                map.get_mut(section_key)
-            }).map(|sub_map| {
-                partition.captures(line).map(|groups| {
-                    let key = groups["key"].trim();
-                    let value = &groups["value"];
-                    sub_map.insert(key.to_string(), value.to_string());
-                })
-            });
-        }
-        map
-    }))
+    Ok(input_str
+        .split(separator.unwrap_or("\r\n"))
+        .fold(HashMap::new(), |mut map, line| {
+            if section.is_match(line) {
+                let section_key = rear_colon.replace_all(line, "").into_owned();
+                map.entry(section_key.clone()).or_insert(HashMap::new());
+                last_section_key.replace(section_key);
+            } else if record.is_match(line) {
+                last_section_key
+                    .as_ref()
+                    .and_then(|section_key| map.get_mut(section_key))
+                    .map(|sub_map| {
+                        partition.captures(line).map(|groups| {
+                            let key = groups["key"].trim();
+                            let value = &groups["value"];
+                            sub_map.insert(key.to_string(), value.to_string());
+                        })
+                    });
+            }
+            map
+        }))
 }
 #[cfg(test)]
 mod test {
     #[test]
-    fn test_build_map(){
+    fn test_build_map() {
         const SOURCE_TEXT: &str = r"
 Windows IP 配置
 
